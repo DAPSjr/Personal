090cf38f1896a24533f518c0a95bb8e7d1db6eb2
DAPSjr <alarconf457@gmail.com>
2022-10-09 17:26:56 -0300

    Se agrega el juego test #1 de python

---
+++ madlibs.py
@@ -0,0 +1,14 @@
+organización = "freeCodeCamp"
+
+print(f"Aprende a programar con {organización}") # f-string
+
+# Mad Libs (Historias Locas)
+
+adj = input("Advetivo: ")
+words_1 = input("Palabra: ")
+words_2 = input("Palabra: ")
+sustantivo_plural = input("Sustantivo (plural): ")
+
+madlib = f"¡El día de ayer fue tan {adj}!, no te das una idea de la cantidad de {words_1} que encontré. Al final me terminé {words_2} todo eso, estaba {sustantivo_plural}"
+
+print(madlib)
